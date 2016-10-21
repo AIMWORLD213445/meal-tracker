67f39180d3a32a3a476b6fa6c9220bed1239e435
Brian Dutz <bgdutz@gmail.com>
2016-10-21 12:51:46 -0700

    finish up calorie pipe

--- app/calorie.pipe.ts
+++ app/calorie.pipe.ts
@@ -8,10 +8,24 @@ import {Meal} from './meal.model';
 })
 
 export class CaloriesPipe implements PipeTransform {
-  transform(input: Meal[]) {
+  transform(input: Meal[], selection: string) {
     var output: Meal[] = [];
-    for(var i = 0; i < input.length; i++){
-      
-    }
+      if(selection === 'under-500') {
+        for(var i = 0; i < input.length; i++){
+          if(input[i].calories<500) {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      } else if(selection === 'over-500'){
+          for(var i=0; i<input.length; i++){
+            if(input[i].calories>=500){
+              output.push(input[i]);
+            }
+          }
+          return output;
+      } else{
+        return input;
+      }
   }
 }
