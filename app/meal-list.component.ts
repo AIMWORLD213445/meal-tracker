8b1d0e0427070614040e0aa7a84e74710eda1b22
Brian Dutz <bgdutz@gmail.com>
2016-10-21 10:07:15 -0700

    add eventEmitter as well as input/output to meal list

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -6,11 +6,19 @@ import { Meal } from './meal.model';
   template:`
   <h3> List of Daily Meals</h3>
   <div *ngFor="let currentMeal of childMealList">
+    <h3>{{ currentMeal.name }} </h3>
+    <h4>{{ currentMeal.details }} </h4>
+    <h4>{{ currentMeal.calories }} </h4>
+    <button class="btn btn-success" (click) = "editButtonClicked(currentMeal)"> Edit Meal</button>
   </div>
 
   `
 })
 
 export class MealListComponent {
-
+  @Input() childMealList: Meal[];
+  @Output() clickSender = new EventEmitter();
+  editButtonClicked (mealToEdit: Meal) {
+    this.clickSender.emit(mealToEdit);
+  }
 }
