a1db93a7816d3cf930aabf0b09279a01c1fc2666
Brian Dutz <bgdutz@gmail.com>
2016-10-21 09:06:46 -0700

    add Meal object and imports plus sample array of meals in app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <h1>Meal Tracker</h1>
   `
 })
 
 export class AppComponent {
+  public masterMeal: Meal[] = [
+    new Meal('Pizza', "two slices for lunch", 570),
+    new Meal('Sushi', "A roll and a couple pieces of ngiri for dinner", 500),
+    new Meal('Cereal', "a bowl with almond milk for breakfast", 130)
+  ]
+
 
 }
