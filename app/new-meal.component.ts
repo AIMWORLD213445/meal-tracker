4c110bd327d8fac72ab36071c4c721e8ec1c4753
Brian Dutz <bgdutz@gmail.com>
2016-10-31 08:04:07 -0700

    add README and bug fixes

--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -8,18 +8,18 @@ import { Meal } from './meal.model';
   <h2> Enter a new meal:</h2>
   <div class="form-group">
     <label> Enter meal name:</label>
-    <input required type="text" name="name" class="form-control" #newName >
+    <input  type="text" name="name" class="form-control" #newName >
   </div>
   <div>
     <label> Enter meal details:</label>
-    <input required  name="details" type="text" class="form-control" #newDetails >
+    <input   name="details" type="text" class="form-control" #newDetails >
   </div>
   <br>
   <div>
     <label> Enter meal calorie count:</label>
-    <input required name="calories" type="number" class="form-control"  #newCalories >
+    <input  name="calories" type="number" class="form-control"  #newCalories >
   </div>
-  <button type="submit" class = "btn btn-success submit-button" ng-click="submitted=true" (click) = "
+  <button type="submit" class = "btn btn-success submit-button" (click) = "
     addClicked(newName.value, newDetails.value,  newCalories.value);
     newName.value='';
     newDetails.value='';
